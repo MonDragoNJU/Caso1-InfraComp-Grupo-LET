7eb9b7ed181f4a07d506909b349d8ae6b35d281d
Julian Mondragon <j.mondragon1@uniandes.edu.co>
2025-01-30 17:50:14 -0500

    Agregar mensaje producto

--- Caso 1/src/Producto.java
+++ Caso 1/src/Producto.java
@@ -11,9 +11,7 @@ public class Producto {
     
     public int getId() { return id; }
     public boolean esReproceso() { return esReproceso; }
-    
-    @Override
-    public String toString() {
-        return "Producto " + id;
-    }
+
+    public String getMensaje() { return mensaje; }
+
 }
